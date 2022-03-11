592d4b49d2e2362e7d15ca7b9f734421ba7abcc1
AndreBertoDev <andre.bertogalli@outlook.it>
2022-03-11 17:31:25 +0100

    k

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,16 +4,19 @@ import AuthContext from "../Contexts/AuthContext";
 import * as Constants from '../Constants';
 import { useNavigate } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import bgImg from '../images/background.gif'
 
 const Login = () => {
     const { setAuth } = useContext(AuthContext);
@@ -71,14 +74,29 @@ const Login = () => {
         mode: 'dark',
       },
     });
-
     return (
-        <ThemeProvider theme={theme}>
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: 'url(' + bgImg +')',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
             <Box
               sx={{
-                marginTop: 8,
+                my: 8,
+                mx: 4,
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
@@ -90,15 +108,15 @@ const Login = () => {
               <Typography component="h1" variant="h5">
                 Sign in
               </Typography>
-              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                 <TextField
                   margin="normal"
                   required
+                  fullWidth
                   ref = {userRef}
                   value = {user}
                   onChange={(e) => setUser(e.target.value)}
                   error = { isFormInvalid }
-                  fullWidth
                   id="username"
                   label="Username"
                   name="username"
@@ -119,27 +137,37 @@ const Login = () => {
                   id="password"
                   autoComplete="current-password"
                 />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
                 <Button
                   type="submit"
                   fullWidth
                   variant="contained"
                   sx={{ mt: 3, mb: 2 }}
                 >
-                  Login
+                  Sign In
                 </Button>
                 <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
                   <Grid item>
                     <Link href="#" variant="body2">
                       {"Don't have an account? Sign Up"}
                     </Link>
                   </Grid>
                 </Grid>
+                <Copyright sx={{ mt: 5 }} />
               </Box>
             </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
-          </Container>
-        </ThemeProvider>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
     );
-};
+  }
 
 export default Login;
